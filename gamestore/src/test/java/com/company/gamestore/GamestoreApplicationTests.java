37d69202c84f2e20edd9542f2ab53d79d9729eaf
marco-mart <martinez.marco.1027@gmail.com>
2023-03-25 10:21:37 -0600

    initial commit

---
+++ gamestore/src/test/java/com/company/gamestore/GamestoreApplicationTests.java
@@ -0,0 +1,16 @@
+package com.company.gamestore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GamestoreApplicationTests {
+
+
+
+	@Test
+	void contextLoads() {
+	}
+
+}
