6b2871ba818bd52957897587a954746193dd0a53
treyduffus <105085785+treyduffus@users.noreply.github.com>
2023-03-26 11:02:41 -0400

    finishing repo tests

--- gamestore/src/test/java/com/company/gamestore/repositories/GameRepositoryTest.java
+++ gamestore/src/test/java/com/company/gamestore/repositories/GameRepositoryTest.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -28,7 +29,7 @@ public class GameRepositoryTest {
     }
 
     @Test
-    public void ShouldCreateABook () throws Exception {
+    public void ShouldCreateAGame () throws Exception {
 
         //Arrange
         Game game = new Game();
@@ -51,7 +52,7 @@ public class GameRepositoryTest {
     }
 
     @Test
-    public void ShouldGetABookById () throws Exception {
+    public void ShouldGetAGameById () throws Exception {
 
         //Arrange
         Game game = new Game();
@@ -74,7 +75,7 @@ public class GameRepositoryTest {
     }
 
     @Test
-    public void ShouldGetAllBooks () throws Exception {
+    public void ShouldGetAllGames () throws Exception {
 
         //Arrange
         Game game = new Game();
@@ -98,7 +99,120 @@ public class GameRepositoryTest {
     }
 
     @Test
-    public void ShouldUpdateABook () throws Exception {
+    public void ShouldUpdateAGame () throws Exception {
+
+        //Arrange
+        Game game = new Game();
+        game.setTitle("Minecraft");
+        game.setEsrbRating("Teen");
+        game.setDescription("Dont mine at night");
+        BigDecimal price = new BigDecimal("9.99");
+        game.setPrice(price);
+        game.setStudio("Microsoft");
+        game.setQuantity(21);
+
+        //Act
+        game = GameRepo.save(game);
+
+        game.setTitle("VALORANT");
+
+        Game newGame = GameRepo.save(game);
+
+        //Assert
+        assertEquals(game,newGame);
+
+    }
+
+    @Test
+    public void ShouldDeleteAGameById() throws Exception {
+
+        //Arrange
+        Game game = new Game();
+        game.setTitle("Minecraft");
+        game.setEsrbRating("Teen");
+        game.setDescription("Dont mine at night");
+        BigDecimal price = new BigDecimal("9.99");
+        game.setPrice(price);
+        game.setStudio("Microsoft");
+        game.setQuantity(21);
+
+        //Act
+        game = GameRepo.save(game);
+
+        GameRepo.deleteById(game.getId());
+
+        Optional<Game> game1 = GameRepo.findById(game.getId());
+
+        //Assert
+        assertFalse(game1.isPresent());
+
+    }
+
+    @Test
+    public void ShouldGetGamesByStudio() throws Exception {
+
+        //Arrange
+        Game game = new Game();
+        game.setTitle("Minecraft");
+        game.setEsrbRating("Teen");
+        game.setDescription("Dont mine at night");
+        BigDecimal price = new BigDecimal("9.99");
+        game.setPrice(price);
+        game.setStudio("Microsoft");
+        game.setQuantity(21);
+
+        //Act
+        game = GameRepo.save(game);
+
+        List<Game> gameList = GameRepo.findAllByStudio("Microsoft");
+
+        //Assert
+        assertEquals(gameList.size(),1);
+
+    }
+
+    @Test
+    public void ShouldGetGamesByEsrbRating() throws Exception {
+
+        //Arrange
+        Game game = new Game();
+        game.setTitle("Minecraft");
+        game.setEsrbRating("Teen");
+        game.setDescription("Dont mine at night");
+        BigDecimal price = new BigDecimal("9.99");
+        game.setPrice(price);
+        game.setStudio("Microsoft");
+        game.setQuantity(21);
+
+        //Act
+        game = GameRepo.save(game);
+
+        List<Game> gameList = GameRepo.findAllByEsrbRating("Teen");
+
+        //Assert
+        assertEquals(gameList.size(),1);
+
+    }
+
+    @Test
+    public void ShouldGetAllGamesByTitle () throws Exception {
+        //Arrange
+        Game game = new Game();
+        game.setTitle("Minecraft");
+        game.setEsrbRating("Teen");
+        game.setDescription("Dont mine at night");
+        BigDecimal price = new BigDecimal("9.99");
+        game.setPrice(price);
+        game.setStudio("Microsoft");
+        game.setQuantity(21);
+
+        //Act
+        game = GameRepo.save(game);
+
+        List<Game> gameList = GameRepo.findAllByTitle("Minecraft");
+
+        //Assert
+        assertEquals(gameList.size(),1);
 
     }
 
