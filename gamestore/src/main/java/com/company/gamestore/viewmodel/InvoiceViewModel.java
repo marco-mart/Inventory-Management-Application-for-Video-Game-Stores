0e403c00efc55f12e3ffe2818e88e6a1617874a4
bridgetorres <bridgetorres01@gmail.com>
2023-04-03 19:06:53 -0400

    Hey

---
+++ gamestore/src/main/java/com/company/gamestore/viewmodel/InvoiceViewModel.java
@@ -0,0 +1,162 @@
+package com.company.gamestore.viewmodel;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+public class InvoiceViewModel {
+    private Integer id;
+
+    private String name;
+
+    private String street;
+
+    private String city;
+
+    private String state;
+
+    private String zipcode;
+
+    private String itemType;
+
+    private Integer itemId;
+
+    private Integer quantity;
+
+   private BigDecimal subtotal;
+
+    private BigDecimal tax;
+
+    private BigDecimal processingFee;
+
+    private BigDecimal total;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+    public String getItemType() {
+        return itemType;
+    }
+
+    public void setItemType(String itemType) {
+        this.itemType = itemType;
+    }
+
+    public Integer getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(Integer itemId) {
+        this.itemId = itemId;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public BigDecimal getTax() {
+        return tax;
+    }
+
+    public void setTax(BigDecimal tax) {
+        this.tax = tax;
+    }
+
+    public BigDecimal getProcessingFee() {
+        return processingFee;
+    }
+
+    public void setProcessingFee(BigDecimal processingFee) {
+        this.processingFee = processingFee;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        InvoiceViewModel that = (InvoiceViewModel) o;
+        return getId() == that.getId() &&
+                Objects.equals(getName(), that.getName()) &&
+                Objects.equals(getStreet(), that.getStreet()) &&
+                Objects.equals(getCity(), that.getCity()) &&
+                Objects.equals(getState(), that.getState()) &&
+                Objects.equals(getZipcode(), that.getZipcode()) &&
+                Objects.equals(getItemType(), that.getItemType()) &&
+                Objects.equals(getItemId(), that.getItemId()) &&
+                Objects.equals(getQuantity(), that.getQuantity()) &&
+                Objects.equals(getSubtotal(), that.getSubtotal()) &&
+                Objects.equals(getTax(), that.getTax()) &&
+                Objects.equals(getProcessingFee(), that.getProcessingFee()) &&
+                Objects.equals(getTotal(), that.getTotal());
+
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, street, city, state, zipcode, itemType, itemId, quantity, subtotal, tax, processingFee, total);
+    }
+}
+
